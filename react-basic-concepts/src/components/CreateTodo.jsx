9694aedab2ead276dcbb3c3c8285e27ff13e1a51
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-03-19 20:48:12 +0430

    Update CreateTodo.jsx

--- react-basic-concepts/src/components/CreateTodo.jsx
+++ react-basic-concepts/src/components/CreateTodo.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateTodo = () => {
+    const [count, setCount] = useState(0);
+    const [input, setInput] = useState("");
+    const tasks = ["Task 1", "Task 2", "Task 3"];
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
+
     return (
         <>
-            <h1>Create new Todo from here</h1>
-            <button>Add Task</button>
+            <h1>Tasks: {count}</h1>
+            <input type="text" onChange={handleChange} />
+            <button onClick={handleClick} value="Add Task Button">
+                Add Task
+            </button>
+            <h1>Input: {input}</h1>
+            {
+                <ul>
+                    {tasks.map((task, i) => (
+                        <li key={task}>{task}</li>
+                    ))}
+                </ul>
+            }
         </>
     );
 };
