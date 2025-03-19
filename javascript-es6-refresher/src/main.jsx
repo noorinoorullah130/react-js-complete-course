c85e0006208810985476d8bdf6694419e50a771c
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-03-19 12:37:04 +0430

    nnn

--- javascript-es6-refresher/src/main.jsx
+++ javascript-es6-refresher/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+const arr1 = [1, 2, 3, 4];
+const arr2 = [7, 8, 9, 10];
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const arr3 = [5, 6];
+
+// const numbers = arr1.concat(arr3, arr2);
+const numbers = [0, ...arr1, 5, 6, ...arr2];
+
+console.log(numbers);
+
+// Objects with spread operator
+
+const obj1 = {
+    name: "Meet",
+};
+
+const obj2 = {
+    hobby: ["Teaching", "Learning"],
+};
+
+const user = { ...obj1, ...obj2, YouTube: "Code Bless You" };
+
+console.log(user);
